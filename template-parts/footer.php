9b978a9a99638d5c801f9d958159929364699584
nach94 <123021458+nach94@users.noreply.github.com>
2024-05-20 00:37:13 -0300

    Some Details 20/05/2024

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,10 +1,10 @@
 <footer class="container_page_footer">
     <div>
-        <a class="container_sing" href="https://helloeveryone.me/" rel="noopener">
+        <a target="_blank" class="container_sing" href="https://helloeveryone.me/" rel="noopener">
             By: <strong>HelloEveryone</strong>
         </a>
-        <a class="container_github" href="https://github.com/nach94/the-next-mcu-movie" rel="noopener">
-            View<strong>Github repository</strong>.
+        <a target="_blank" class="container_github" href="https://github.com/nach94/the-next-mcu-movie" rel="noopener">
+            View<strong> Github repository</strong>.
         </a>
     </div>
 </footer>
